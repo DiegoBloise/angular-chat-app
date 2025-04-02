2523b9b9f1c800c41074ffb9488261c29f82af2e
Diego Bloise <diego-dev@outlook.com>
2025-04-02 03:44:39 -0300

    update: chat list

--- src/app/components/chat-list/chat-list.component.ts
+++ src/app/components/chat-list/chat-list.component.ts
@@ -3,15 +3,17 @@ import { Router } from '@angular/router';
 import { Chat } from '../../models/chat.model';
 import { ChatService } from '../../services/chat.service';
 import { ChatCardComponent } from '../chat-card/chat-card.component';
-
+import { TabsModule } from 'primeng/tabs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-chat-list',
   templateUrl: './chat-list.component.html',
   styleUrls: ['./chat-list.component.scss'],
-  imports: [ChatCardComponent],
+  imports: [ChatCardComponent, TabsModule, CommonModule],
 })
 export class ChatListComponent implements OnInit {
   chats!: Chat[];
+  tabs: { title: string; value: number; chats: Chat[] }[] = [];
 
   constructor(private router: Router, private chatService: ChatService) {}
 
@@ -21,6 +23,16 @@ export class ChatListComponent implements OnInit {
       .sort(
         (a, b) => b.lastMessageDate.getTime() - a.lastMessageDate.getTime()
       );
+
+    this.tabs = [
+      { title: 'All', value: 0, chats: this.chats },
+      { title: 'Unread', value: 1, chats: this.chats },
+      { title: 'Personal', value: 3, chats: this.chats },
+      { title: 'Family', value: 4, chats: this.chats },
+      { title: 'Friends', value: 4, chats: this.chats },
+      { title: 'Work', value: 5, chats: this.chats },
+      { title: 'Bots', value: 6, chats: this.chats },
+    ];
   }
 
   navigateToChat(userId: string | undefined): void {
